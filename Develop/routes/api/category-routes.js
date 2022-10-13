dd1bef834efaa83a0e5656afc324af7518d6400a
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-10-13 00:13:46 -0400

    Update category-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,16 +5,40 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+   Category.findAll({
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'category_id']
+    }
+  })
   // be sure to include its associated Products
+    .then(dbCategoryData => res.json(dbCategoryData))
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+   Category.findOne({
+    where: {
+      id: req.id
+    },
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'category_id']
+    }
+  })
+// be sure to include its associated Products
+    if(!dbCategoryData) {
+      res.status(404).json({ message: 'No category found with this id' });
+    res.json(dbCategoryData);
+  })
+  
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name;
+  })
 });
 
 router.put('/:id', (req, res) => {
