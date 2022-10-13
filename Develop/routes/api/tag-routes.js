b7c000d8c4938b70a07ff65edb8356f831134cf1
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-10-13 00:08:07 -0400

    Update tag-routes.js

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -4,13 +4,41 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: {
+      model: Product,
+      attributes: ['product_name', 'price', 'category_id']
+    }
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.json(500).json(err);
+  });
 });
 
+
+
 router.get('/:id', (req, res) => {
+  
   // find a single tag by its `id`
+   Tag.findOne({
+    where: {
+      id: req.id
+    },
+    include: {
+      model: Product,
+      attributes: ['product_name', 'price', 'category_id']
+    }
+  })
   // be sure to include its associated Product data
+  
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag found with this id' });
+      return;
+    res.json(dbTagData);
+  })
 });
 
 router.post('/', (req, res) => {
